1967d71a847ce7257d2c2009f8ec50144cef2930
Thura Freelance <mozet89@gmail.com>
2024-04-13 17:36:25 +0800

    fix address

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -13,9 +13,9 @@ const Footer = () => (
     <div className="app__footer-links">
       <div className="app__footer-links_contact">
         <h1 className="app__footer-headtext">Contact Us</h1>
-        <p className="p__opensans">9 W 53rd St, New York, NY 10019, USA</p>
-        <p className="p__opensans">+1 212-344-1230</p>
-        <p className="p__opensans">+1 212-555-1230</p>
+        <p className="p__opensans">9 W 53rd St, Yangon, Burmese, Myanmar</p>
+        <p className="p__opensans">yuzanakyawdaiichi@gmail.com</p>
+        <p className="p__opensans">+959 212-555-1230</p>
       </div>
 
       <div className="app__footer-links_logo">
@@ -46,7 +46,7 @@ const Footer = () => (
       </div>
     </div>
     <div className="footer__copyright">
-      <p className="p__opensans">2021 Gericht. All Rights reserved.</p>
+      <p className="p__opensans">2024 Yuzana YK. All Rights reserved.</p>
     </div>
   </div>
 );
